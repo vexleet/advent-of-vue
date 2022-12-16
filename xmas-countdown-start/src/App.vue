9a48291d96730f3047f44f4a140ef1ae201d14a7
vexleet <abda70226@gmail.com>
2022-12-16 14:24:51 +0100

    start 3rd challenge

---
+++ xmas-countdown-start/src/App.vue
@@ -0,0 +1,60 @@
+<script setup>
+import CountdownHeader from '@/components/CountdownHeader.vue'
+import CountdownSegment from './components/CountdownSegment.vue'
+import { useNow } from '@vueuse/core'
+import {ref, watch} from "vue";
+
+const now = useNow({interval: 1000})
+const christmas = new Date('12/25/2022 00:00:00')
+
+const time = ref({
+  days: 0,
+  hours: 0,
+  minutes: 0,
+  seconds: 0
+})
+
+watch(now, newVal => {
+  const diffTime = Math.abs(newVal.getTime() - christmas.getTime())
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  const diffHours = Math.ceil((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const diffMinutes = Math.ceil((diffTime % (1000 * 60 * 60)) / (1000 * 60));
+  const diffSeconds = Math.ceil((diffTime % (1000 * 60)) / 1000);
+
+  time.value = {
+    days: diffDays,
+    hours: diffHours,
+    minutes: diffMinutes,
+    seconds: diffSeconds
+  }
+})
+console.log(now)
+</script>
+<template>
+  <div class="w-full h-full flex justify-center items-center p-10">
+    <div>
+      <div class="shadow-md relative bg-white p-5 rounded-lg border-gray-100 border-[1px]">
+        <CountdownHeader />
+        <main class="flex justify-center">
+          <CountdownSegment label="days" :number="time.days" />
+          <CountdownSegment label="hours" :number="time.hours" />
+          <CountdownSegment label="minutes" :number="time.minutes" />
+          <CountdownSegment label="seconds" :number="time.seconds" />
+        </main>
+      </div>
+      <h4 class="mt-10 text-gray-400 text-center text-sm">
+        This challenge brought to you by <a href="https://vueschool.io/" class="underline">Vue School</a>
+      </h4>
+    </div>
+  </div>
+</template>
+
+<style>
+div {
+  display: block;
+}
+
+body {
+  @apply bg-gray-100;
+}
+</style>
