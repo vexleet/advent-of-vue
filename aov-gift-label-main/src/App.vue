e6603f68f7f4ab28533c57001389196bff272d84
vexleet <abda70226@gmail.com>
2022-12-17 20:31:56 +0100

    add gift label challenge and add global .gitignore

---
+++ aov-gift-label-main/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="w-full h-full flex flex-col justify-center items-center">
+    <div>
+      <div class="w-full max-w-2xl p-4 rounded bg-red-400 text-white mb-6">
+        <div class="border-2 rounded flex flex-col md:flex-row md:items-center md:gap-8 p-4">
+          <img src="/assets/santa.svg" alt="" class="max-w-[12em]" />
+          <div>
+            <h1 class="font-serif text-4xl">Happy Holidays!</h1>
+            <div class="mt-4 bg-white text-black p-2 rounded">TO: {{ toName }}</div>
+            <div class="mt-2 bg-white text-black p-2 rounded">FROM: {{ fromName }}</div>
+          </div>
+        </div>
+      </div>
+
+      <InputField v-model:value="toName" label="To:" id="toName"/>
+      <InputField v-model:value="fromName" label="From:" id="fromName"/>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+import InputField from "@/components/InputField.vue";
+
+const toName = ref('')
+const fromName = ref('')
+</script>
---
+++ aov-gift-label-main/src/components/InputField.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="d-block self-start">
+    <label :for="id">{{ label }}</label>
+    <input :value="value" @input="$emit('update:value', $event.target.value)" type="text" :id="id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" >
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  label: {type: String, required: true},
+  id: {type: String, required: true},
+  value: {type: String, required: true}
+})
+
+defineEmits(['update:value'])
+</script>
+
+<style scoped>
+
+</style>
