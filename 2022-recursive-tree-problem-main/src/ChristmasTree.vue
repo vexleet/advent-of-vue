37117da4fb019217f6a241001fec960a04cf96c7
vexleet <abda70226@gmail.com>
2022-12-17 20:15:28 +0100

    add recursive tree problem challenge (used hints.md)

---
+++ 2022-recursive-tree-problem-main/src/ChristmasTree.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="size >= 0">
+      <ChristmasTree :size="size - 1">
+        <template #default>
+          <div class="flex flex-row justify-center">
+            <!-- This div is styled for the tree sections, but it's not complete -->
+            <div v-for="i in size" class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
+          </div>
+        </template>
+      </ChristmasTree>
+      <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    size: number
+  }>(),
+  {
+    size: 1,
+  }
+)
+</script>
